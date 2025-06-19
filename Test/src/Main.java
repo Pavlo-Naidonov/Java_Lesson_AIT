4798aeb243ea0b36a81411667ac48b1b2699e035
Pavlo-Naidonov <gargoyle9293@gmail.com>
2025-06-19 16:46:17 +0200

    add all

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        int j = 10;
+        System.out.println("j=" + j);
+        //System.out.println("j++ = " + j++); // постфиксная операция
+        System.out.println("++j = " + ++j);  // префиксная
+        System.out.println("j = " + j);
 
-        double a;
-        char op;
-        double b;
 
-        double result;
+                    /*
+            System.out.println(j--); // сначала вывод на экран j, потом j = j - 1;
+            System.out.println(--j); // сначала j = j - 1 потом вывод на экран j
 
+             */
 
     }
 }
